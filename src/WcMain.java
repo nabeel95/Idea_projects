eba2eb160cc09e69e1f7dbf0133010e0fdb42426
nabeel95 <nabeel@thoughtworks.com>
2016-02-23 08:00:36 +0530

    byte count completed

--- src/WcMain.java
+++ src/WcMain.java
@@ -3,30 +3,43 @@ import java.io.FileReader;
 import java.util.Arrays;
 
 class WcMain  {
+	public static String data(String fileName) throws Exception{
+		FileReader file = new FileReader(fileName);
+		BufferedReader reader = new BufferedReader(file);
+		String text = "";
+		String line = reader.readLine();
+		int lines=0;
+		while (line!=null) {
+			text+=line+"\n";
+			line=reader.readLine();
+			lines++;
+		}
+		return text;
+	}
+
 	public static void main(String[] args) throws Exception {
-			String minus = "-";
 			String fileName="";
 			if(args[0].charAt(0) != "-".charAt(0)){
 				fileName+=args[0];
-				
-				System.out.println("args = "+args[1].charAt(0));
-				FileReader file = new FileReader(fileName);
-				BufferedReader reader = new BufferedReader(file);
-				String text = "";
-				String line = reader.readLine();
-				int lines=0;
-				while (line!=null) {
-					text+=line;
-					line=reader.readLine();
-					lines++;
-				}
-
-				WordCount str = new WordCount(text);
+			}else {
+				fileName+=args[1];
+			}
+			String text = data(fileName);
+			WordCount str = new WordCount(text);
+			if(args[0].length()==2 && args[0].charAt(0) == "-".charAt(0) && args[0].charAt(1) == "l".charAt(0)){
+				System.out.println("Lines = "+str.getLineCount());
+			}
+			else if(args[0].length()==2 && args[0].charAt(0) == "-".charAt(0) && args[0].charAt(1) == "w".charAt(0)){
+				System.out.println("Words = "+str.getWordCount());
+			}
+			else if(args[0].length()==2 && args[0].charAt(0) == "-".charAt(0) && args[0].charAt(1) == "c".charAt(0)){
+				System.out.println("Characters = "+str.getCharCount());
+			}else{
 				System.out.println("Characters = "+str.getCharCount());
 				System.out.println("Words = "+str.getWordCount());
-				System.out.println("Lines = "+lines);
-			}else{
-				System.out.println("wrong");
+				System.out.println("Lines = "+str.getLineCount());
+				System.out.println("Bytes = "+str.getByteCount());
+
 			}
 	}
 }
--- src/WordCount.java
+++ src/WordCount.java
@@ -1,17 +1,28 @@
+
 public class WordCount{
 	private String str;
+
 	public WordCount(String str){
 		this.str=str;
 	}
+
 	public int getCharCount(){
 		return this.str.length();
 	}
+
+	public int getByteCount() throws Exception{
+		return this.str.getBytes("UTF-8").length;
+	}
+
 	public int getWordCount(){
 		String trimmed = this.str.trim();
 		int words = trimmed.isEmpty() ? 0 : trimmed.split("\\s+").length;
 		return words;
 	}
+
 	public int getLineCount(){
-		return 0;
+		String[] string = this.str.split("\n");
+		return string.length;
 	}
+
 }
