f73fab8c9a6079f4de22d62d1278088e4d98929b
nabeel95 <nabeel@thoughtworks.com>
2016-03-02 12:21:19 +0530

    wc working properly

--- src/WcMain.java
+++ src/WcMain.java
@@ -1,32 +1,33 @@
 import java.io.File;
 import java.util.ArrayList;
-class WcMain  {
-	public static void main(String[] args) throws Exception {
-		ArrayList<String> files = new ArrayList<>();
-		ArrayList<String> commands = new ArrayList<>();
-		ArrayList<String> wrongFiles = new ArrayList<>();
-		for (String arg : args) {
-			File f = new File(arg);
-			if (f.exists()) {
-				files.add(arg);
-			}else if(arg.charAt(0)=="-".charAt(0)) {
-				commands.add(arg);
-			}else {
-				wrongFiles.add(arg);
-			}
-		}
-		if(commands.size()==0){
-			PrintWithoutCommands res = new PrintWithoutCommands(files);
-			res.printResult();
-		}else {
-			PrintAccordingCommand res = new PrintAccordingCommand(files,commands);
-			res.printResult();
-		}
-		if(wrongFiles.size()>0){
-			for(int i=0;i<wrongFiles.size();i++){
-				System.out.println("\t"+wrongFiles.get(i)+" is not a file;");
-			}
-		}
 
-	}
+class WcMain {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> files = new ArrayList<>();
+        ArrayList<String> commands = new ArrayList<>();
+        ArrayList<String> wrongFiles = new ArrayList<>();
+        for (String arg : args) {
+            File f = new File(arg);
+            if (f.exists()) {
+                files.add(arg);
+            } else if (arg.charAt(0) == "-".charAt(0)) {
+                commands.add(arg);
+            } else {
+                wrongFiles.add(arg);
+            }
+        }
+        if (commands.size() == 0) {
+            PrintWithoutCommands res = new PrintWithoutCommands(files);
+            res.printResult();
+        } else {
+            PrintAccordingCommand res = new PrintAccordingCommand(files, commands);
+            res.printResult();
+        }
+        if (wrongFiles.size() > 0) {
+            for (String wrongFile : wrongFiles) {
+                System.out.println("\t" + wrongFile + " is not a file;");
+            }
+        }
+
+    }
 }
--- src/WordCount.java
+++ src/WordCount.java
@@ -1,28 +1,27 @@
 
-public class WordCount{
-	private String str;
+public class WordCount {
+    private String str;
 
-	public WordCount(String str){
-		this.str=str;
-	}
+    public WordCount(String str) {
+        this.str = str;
+    }
 
-	public int getCharCount(){
-		return this.str.length();
-	}
+    public int getCharCount() {
+        return this.str.length();
+    }
 
-	public int getByteCount() throws Exception{
-		return this.str.getBytes("UTF-8").length;
-	}
+    public int getByteCount() throws Exception {
+        return this.str.getBytes("UTF-8").length;
+    }
 
-	public int getWordCount(){
-		String trimmed = this.str.trim();
-		int words = trimmed.isEmpty() ? 0 : trimmed.split("\\s+").length;
-		return words;
-	}
+    public int getWordCount() {
+        String trimmed = this.str.trim();
+        return trimmed.isEmpty() ? 0 : trimmed.split("\\s+").length;
+    }
 
-	public int getLineCount(){
-		String[] string = this.str.split("\n");
-		return string.length;
-	}
+    public int getLineCount() {
+        String[] string = this.str.split("\n");
+        return string.length - 1;
+    }
 
 }
